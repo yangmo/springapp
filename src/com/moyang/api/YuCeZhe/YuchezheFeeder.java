73743808488d898506cf4da8e6e213de84584989
Yang <moyang@amazon.com>
2015-08-04 23:47:29 +0800

    add Marketplace Data and update to 20150804

--- src/com/moyang/api/YuCeZhe/YuchezheFeeder.java
+++ src/com/moyang/api/YuCeZhe/YuchezheFeeder.java
@@ -2,10 +2,13 @@ package com.moyang.api.YuCeZhe;
 
 import java.io.File;
 import java.util.List;
+import java.util.Set;
 
 import com.moyang.common.Constants;
 import com.moyang.common.FileUtil;
+import com.moyang.common.StockNameUtil;
 import com.moyang.hibernate.StockDaily;
+
 import org.springframework.util.StringUtils;
 
 /**
@@ -65,6 +68,23 @@ public class YuchezheFeeder {
         }
         FileUtil.writeToFile(path, sb.toString());
     }
+    
+    static void printMissingStockIds(String date) {
+		String contents = FileUtil.getContent(getStockFeedPath(date));
+		String[] lines = contents.split("\n");
+		
+		for(int i = 1; i < lines.length; i++) {
+			try {
+			    String stockId = YucezheCommonUtil.getStockIdFromLine(lines[i]);
+			    Set<String> allStockIds = StockNameUtil.getAllStockIds();
+			    if(!allStockIds.contains(stockId)) {
+			    	System.out.println("Missing " + stockId);
+			    }
+			} catch (Throwable t) {
+				System.out.println(t.getLocalizedMessage());
+			}
+		}
+    }
 	static void feedAll(String date) {
 		String contents = FileUtil.getContent(getStockFeedPath(date));
 		String[] lines = contents.split("\n");
@@ -72,8 +92,6 @@ public class YuchezheFeeder {
 		for(int i = 1; i < lines.length; i++) {
 			try {
 			    String stockId = YucezheCommonUtil.getStockIdFromLine(lines[i]);
-			    String currentData = FileUtil.getContent(YucezheCommonUtil.getDataPath(stockId));
-			    boolean endWithEnter = currentData.endsWith("\n");
 			
 			    StockDaily feed = YucezheCommonUtil.getStockDailyFromLine(lines[i]);
 			    List<StockDaily> stockDailies = YucezheAPI.getStockDailies(stockId);
@@ -90,10 +108,11 @@ public class YuchezheFeeder {
 				System.out.println(t.getLocalizedMessage());
 			}
 		}
-		
+		printMissingStockIds(date);
 	}
 	
 	public static void main(String[] args) {
-		feedAll("20150804");
+		//feedAll("20150804");
+		printMissingStockIds("20150804");
 	}
 }
--- src/com/moyang/common/StockNameUtil.java
+++ src/com/moyang/common/StockNameUtil.java
@@ -1,22 +1,25 @@
 package com.moyang.common;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Set;
 
 public class StockNameUtil {
 
+	private static final String MARKETPLACE_BASE = "data/marketplace data/";
+	private static final String[] MARKETPLACES = {"sh", "sz", "zx", "cy"};
 	public static final HashMap<String, String> STOCK_NAME_MAP = new HashMap<String, String>();
 	
 	protected StockNameUtil(){
 	}
 
 	static {
-		init();
+		for (String marketplace : MARKETPLACES) {
+			loadMarketplace(marketplace);
+		}
+
 	}
-	
-	public static void init(){
-		String content =  FileUtil.getContent(Constants.STOCKS_TXT);
+	static void loadMarketplace(String marketplace) {
+		String content =  FileUtil.getContent(MARKETPLACE_BASE + marketplace);
 
 		String[] stocks = content.split("\n");
 		for(String stock : stocks){
@@ -25,27 +28,18 @@ public class StockNameUtil {
 			if(tmp == null || tmp.length != 2){
 				continue;
 			}
-
-
 			STOCK_NAME_MAP.put(tmp[0], tmp[1]);
 		}
 
 	}
 
-	public static void main(String[] args){
-		init();
-	}
+
+
 	public static String getName(String stockId){
-		if(STOCK_NAME_MAP.size() == 0){
-			init();
-		}
 		return STOCK_NAME_MAP.get(stockId).replace("*", "");
 	}
 	
 	public static Set<String> getAllStockIds(){
-	    if(STOCK_NAME_MAP.size() == 0){
-	    	init();
-	    }
 	    return STOCK_NAME_MAP.keySet();
 	}
 }
